e733da4a0c903a3610c9d161ecca998dc4c50f0f
nomoresqt <qsong2610@gmail.com>
2022-04-24 08:53:09 +0200

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -84,7 +84,7 @@ def build_model():
 
     parameters = {'vect__min_df': [1, 5],
                 'tfidf__use_idf': [True, False],
-                'clf__estimator__n_estimators': [10, 25],
+                'clf__estimator__n_estimators': [200, 500],
                 'clf__estimator__min_samples_split': [2, 4]}
 
     cv = GridSearchCV(pipeline, param_grid=parameters)
